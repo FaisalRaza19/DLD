ec49bd56049145d39813f402eb89ae6b0f791fc6
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-08-24 21:03:37 +0500

    done

--- Backend/DataBase/db.js
+++ Backend/DataBase/db.js
@@ -1,10 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectToDb = async()=>{
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log("Data base connect successfully.");
-    } catch (error) {
-        console.log("Failed to connect db",error.message)
-    }
-}
+export const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Failed to connect to DB:", error.message);
+  }
+};
